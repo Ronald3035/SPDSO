79fef5632d9fdb8146c32c18080fd84a4f5023b5
Ronald Delgado <ronalddelgado3035@gmail.com>
2021-06-11 21:59:59 -0400

    2do parcial

---
+++ Controllers/TextilesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiTextiles.Context;
+
+namespace ApiTextiles.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TextilesController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public TextilesController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            //Error handling Try-catch
+            return Ok(context.textiles.ToList());
+        }
+
+    }
+}
---
+++ Models/Textiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTextiles.Models
+{
+    public class Textiles
+    {
+        [Key]
+        public int id { get; set; }
+        public string marca { get; set; }
+        public string ancho { get; set; }
+        public string largo { get; set; }
+        public string tipo { get; set; }
+        public string color { get; set; }
+        public string pais { get; set; }
+        public bool estado { get; set; }
+    }
+}
